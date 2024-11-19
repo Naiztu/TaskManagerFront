447f94e9c93ca21581ed509e81227ed6d08f7a77
= <=>
2024-11-19 16:46:24 -0600

    Fix import path for Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 
